ad625bbe143bb20c71ad86098ebc0bd9ddbb1849
kasgau <phzz204208@gmail.com>
2024-09-17 18:49:43 +0700

    init file EX4

---
+++ SOURCE_CODE/Core/Inc/EX4.h
@@ -0,0 +1,14 @@
+/*
+ * EX4.h
+ *
+ *  Created on: Sep 17, 2024
+ *      Author: kanza
+ */
+
+#ifndef INC_EX4_H_
+#define INC_EX4_H_
+
+#include "main.h"
+void display7SEG(int num);
+
+#endif /* INC_EX4_H_ */
